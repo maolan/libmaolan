3082c8e74a8b4fc2f41aad9663a9b7c820133568
Goran Mekić <meka@tilda.center>
2018-01-10 08:58:52 +0100

    Add newPackage()

--- include/types.h
+++ include/types.h
@@ -10,3 +10,9 @@ typedef std::shared_ptr<BufferData> Buffer;
 typedef std::shared_ptr<std::string> Origin;
 typedef std::pair<Origin, Buffer> PackageData;
 typedef std::shared_ptr<PackageData> Package;
+
+
+Package const newPackage()
+{
+  return Package(new PackageData(new std::string, new BufferData));
+}
--- maolan.cxx
+++ maolan.cxx
@@ -7,12 +7,15 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-  Package package(new PackageData(new string, new BufferData));
-  auto buffer = package->second;
+  auto package = newPackage();
+
   auto origin = package->first;
   *origin = "Origin";
+
+  auto buffer = package->second;
   buffer->push_back(0.5);
   cout << *origin << ' ' << buffer->front() << ' ' << buffer->size() << endl;
+
   cout << "Before " << package.use_count() << endl;
   {
     auto other = package;
