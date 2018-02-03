63c4f30ee85f9f3ed657b7574ab80bb631119b11
Goran Mekić <meka@tilda.center>
2018-02-03 02:02:24 +0100

    Split open function for input and output

--- fullduplex.cxx
+++ fullduplex.cxx
@@ -13,67 +13,33 @@ using namespace std;
 
 
 string dspname = "/dev/dsp";
-
-
 int fd_out = -1, fd_in = -1;
-char buffer[32 * 1024];		/* Max 32k local buffer */
+int dataSize = 32 * 1024;
+int *rawData = new int[dataSize];		/* Max 32k local buffer */
 int rate = 48000;
 int fragsize;
+int channels = 2;
+int frag = 10;
+int format = AFMT_S32_NE;
 
 
-static void open_two_devices(const string &dspname_out, const string &dspname_in)
+static void openInput(const string &dspname)
 {
-  oss_audioinfo ai_in, ai_out;
+  oss_audioinfo ai;
   int tmp;
   int devcaps;
-  int channels = 2;
-  int format;
-  int frag = 6;
 
-  if ((fd_out = open(dspname_out.data(), O_WRONLY, 0)) == -1)
+  if ((fd_in = open(dspname.data(), O_RDONLY, 0)) == -1)
   {
-    cerr << dspname_out;
+    cerr << dspname;
     cerr << strerror(errno) << endl;
     exit(1);
   }
-
-  ai_out.dev = -1;
-  if (ioctl (fd_out, SNDCTL_ENGINEINFO, &ai_out) != -1)
+  ai.dev = -1;
+  if (ioctl(fd_in, SNDCTL_ENGINEINFO, &ai) != -1)
   {
-    cout << "Using audio engine " << ai_out.dev;
-    cout << " = " << ai_out.name << " for output" << endl;
-  }
-  if ((fd_in = open(dspname_in.data(), O_RDONLY, 0)) == -1)
-  {
-    cerr << dspname_in;
-    cerr << strerror(errno) << endl;
-    exit(1);
-  }
-
-  ai_in.dev = -1;
-  if (ioctl (fd_in, SNDCTL_ENGINEINFO, &ai_in) != -1)
-  {
-    cout << "Using audio engine " << ai_in.dev;
-    cout << " = " << ai_in.name << " for output" << endl;
-  }
-
-  if (ai_in.rate_source != ai_out.rate_source)
-  {
-    cerr << "Note! " << dspname_out << " and " << dspname_in << " are not";
-    cerr << " necessarily driven by the same clock." << endl;
-  }
-
-  if (ioctl (fd_out, SNDCTL_DSP_GETCAPS, &devcaps) == -1)
-  {
-    cerr << "SNDCTL_DSP_GETCAPS";
-    cerr << strerror(errno) << endl;
-    exit(1);
-  }
-
-  if (devcaps & PCM_CAP_DUPLEX)
-  {
-    cerr << "Device " << dspname_out << " supports duplex so you may want to";
-    cerr << " use the single device approach instead." << endl;
+    cout << "Using audio engine " << ai.dev;
+    cout << " = " << ai.name << " for output" << endl;
   }
 
   if (ioctl (fd_in, SNDCTL_DSP_GETCAPS, &devcaps) == -1)
@@ -84,13 +50,6 @@ static void open_two_devices(const string &dspname_out, const string &dspname_in
   }
 
   tmp = frag;
-  if (ioctl (fd_out, SNDCTL_DSP_SETFRAGMENT, &tmp) == -1)
-  {
-    cerr << "SNDCTL_DSP_SETFRAGMENT";
-    cerr << strerror(errno) << endl;
-    exit(1);
-  }
-  tmp = frag;
   if (ioctl (fd_in, SNDCTL_DSP_SETFRAGMENT, &tmp) == -1)
   {
     cerr << "SNDCTL_DSP_SETFRAGMENT";
@@ -99,19 +58,6 @@ static void open_two_devices(const string &dspname_out, const string &dspname_in
   }
 
   tmp = channels;
-  if (ioctl (fd_out, SNDCTL_DSP_CHANNELS, &tmp) == -1)
-  {
-    cerr << "SNDCTL_DSP_CHANNELS";
-    cerr << strerror(errno) << endl;
-    exit(1);
-  }
-
-  if (tmp != channels)
-  {
-    cerr << dspname_out << " doesn't support stereo (";
-    cerr << tmp << ")" << endl;
-    exit(1);
-  }
   if (ioctl (fd_in, SNDCTL_DSP_CHANNELS, &tmp) == -1)
   {
     cerr << "SNDCTL_DSP_CHANNELS";
@@ -120,81 +66,117 @@ static void open_two_devices(const string &dspname_out, const string &dspname_in
   }
   if (tmp != channels)
   {
-    cerr << dspname_in << " doesn't support stereo (";
-    cerr << tmp << ")" << endl;
-    exit(1);
-  }
-  tmp = AFMT_S16_NE;
-
-  if (ioctl (fd_out, SNDCTL_DSP_SETFMT, &tmp) == -1)
-  {
-    cerr << "SNDCTL_DSP_SETFMT";
-    cerr << strerror(errno) << endl;
-    exit(1);
-  }
-  if (tmp != AFMT_S16_NE && tmp != AFMT_S16_OE)
-  {
-    cerr << dspname_out << " doesn't support 16 bit sample format (";
+    cerr << dspname << " doesn't support stereo (";
     cerr << tmp << ")" << endl;
     exit(1);
   }
 
-  format = tmp;
+  tmp = format;
   if (ioctl (fd_in, SNDCTL_DSP_SETFMT, &tmp) == -1)
   {
     cerr << "SNDCTL_DSP_SETFMT";
     cerr << strerror(errno) << endl;
     exit(1);
   }
-
-  if (tmp != format)
+  if (tmp == AFMT_S32_OE)
   {
-    cerr << "Error: Input and output devices use different formats ";
-    cerr << "(" << tmp << "/" << format << ")" << endl;
-    exit(1);
+    cerr << "Warning: Using 32 bit sample format with wrong endianess." << endl;
   }
 
-  if (format == AFMT_S16_OE)
-  {
-    cerr << "Warning: Using 16 bit sample format with wrong endianess." << endl;
-  }
   tmp = rate;
-
   if (ioctl (fd_in, SNDCTL_DSP_SPEED, &tmp) == -1)
   {
     cerr << "SNDCTL_DSP_SPEED";
     cerr << strerror(errno) << endl;
     exit(1);
   }
-
   if (tmp != rate)
   {
-    cerr << dspname_out << " doesn't support requested rate ";
+    cerr << dspname << " doesn't support requested rate ";
     cerr << rate << " (" << tmp << ")" << endl;
     exit(1);
   }
+}
 
-  if (ioctl (fd_out, SNDCTL_DSP_SPEED, &tmp) == -1)
+
+static void openOutput(const string &dspname)
+{
+  oss_audioinfo ai;
+  int tmp;
+  int devcaps;
+
+  if ((fd_out = open(dspname.data(), O_WRONLY, 0)) == -1)
+  {
+    cerr << dspname;
+    cerr << strerror(errno) << endl;
+    exit(1);
+  }
+  ai.dev = -1;
+  if (ioctl(fd_out, SNDCTL_ENGINEINFO, &ai) != -1)
+  {
+    cout << "Using audio engine " << ai.dev;
+    cout << " = " << ai.name << " for output" << endl;
+  }
+
+  if (ioctl (fd_out, SNDCTL_DSP_GETCAPS, &devcaps) == -1)
+  {
+    cerr << "SNDCTL_DSP_GETCAPS";
+    cerr << strerror(errno) << endl;
+    exit(1);
+  }
+
+  tmp = frag;
+  if (ioctl(fd_out, SNDCTL_DSP_SETFRAGMENT, &tmp) == -1)
+  {
+    cerr << "SNDCTL_DSP_SETFRAGMENT";
+    cerr << strerror(errno) << endl;
+    exit(1);
+  }
+
+  tmp = channels;
+  if (ioctl(fd_out, SNDCTL_DSP_CHANNELS, &tmp) == -1)
+  {
+    cerr << "SNDCTL_DSP_CHANNELS";
+    cerr << strerror(errno) << endl;
+    exit(1);
+  }
+  if (tmp != channels)
+  {
+    cerr << dspname << " doesn't support stereo (";
+    cerr << tmp << ")" << endl;
+    exit(1);
+  }
+
+  tmp = format;
+  if (ioctl(fd_out, SNDCTL_DSP_SETFMT, &tmp) == -1)
+  {
+    cerr << "SNDCTL_DSP_SETFMT";
+    cerr << strerror(errno) << endl;
+    exit(1);
+  }
+  if (tmp != AFMT_S32_NE && tmp != AFMT_S32_OE)
+  {
+    cerr << dspname << " doesn't support 32 bit sample format (";
+    cerr << tmp << ")" << endl;
+    exit(1);
+  }
+
+  tmp = rate;
+  if (ioctl(fd_out, SNDCTL_DSP_SPEED, &tmp) == -1)
   {
     cerr << "SNDCTL_DSP_SPEED";
     cerr << strerror(errno) << endl;
     exit(1);
   }
 
-  if (tmp != rate)
-  {
-    cerr << dspname_out << " doesn't support the same rate ";
-    cerr << rate << "!=" << tmp << " as " << dspname_in << endl;
-    exit(1);
-  }
-  if (ioctl (fd_in, SNDCTL_DSP_GETBLKSIZE, &fragsize) == -1)
+  if (ioctl(fd_in, SNDCTL_DSP_GETBLKSIZE, &fragsize) == -1)
   {
     cerr << "SNDCTL_DSP_GETBLKSIZE";
     cerr << strerror(errno) << endl;
     exit(1);
   }
 
-  if (fragsize > sizeof (buffer))
+  if (fragsize > dataSize)
   {
     cerr << "Too large fragment size " << fragsize << endl;
     exit(1);
@@ -209,27 +191,27 @@ static void play(int fd_out, int fd_in)
   int l;
   int loopcount = 0;
   char silence[32 * 1024];	/* Buffer for one fragment of silence */
-  while ((l = read (fd_in, buffer, fragsize)) == fragsize)
+  while ((l = read(fd_in, rawData, fragsize)) == fragsize)
   {
     int delay;
     float t;
 
     if (loopcount == 0)
     {
-      memset (silence, 0, fragsize);
-      if (write (fd_out, silence, fragsize) != fragsize)
+      memset(silence, 0, fragsize);
+      if (write(fd_out, silence, fragsize) != fragsize)
       {
         cerr << "write";
         cerr << strerror(errno) << endl;
         exit(1);
       }
     }
-    if (ioctl (fd_out, SNDCTL_DSP_GETODELAY, &delay) == -1) delay = 0;
-    delay /= 4;		/* Get number of 16 bit stereo samples */
-    t = (float) delay / (float) rate;	/* Get delay in seconds */
+    if (ioctl(fd_out, SNDCTL_DSP_GETODELAY, &delay) == -1) delay = 0;
+    delay /= 8;		/* Get number of 16 bit stereo samples */
+    t = (float)delay / (float)rate;	/* Get delay in seconds */
     t *= 1000.0;		/* Convert delay to milliseconds */
 
-    if ((l = write (fd_out, buffer, fragsize)) != fragsize)
+    if ((l = write(fd_out, rawData, fragsize)) != fragsize)
     {
       cerr << "write";
       cerr << strerror(errno) << endl;
@@ -246,8 +228,7 @@ static void play(int fd_out, int fd_in)
 }
 
 
-int
-main (int argc, char *argv[])
+int main (int argc, char **argv)
 {
 
   if (argc > 1) dspname = argv[1];
@@ -256,7 +237,8 @@ main (int argc, char *argv[])
     rate = atoi (argv[3]);
     if (rate == 0) rate = 48000;
   }
-  open_two_devices(dspname, dspname);
+  openInput(dspname);
+  openOutput(dspname);
   play(fd_out, fd_in);
 
   return 0;
