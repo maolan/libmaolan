71cad6cab08edd7fbd6156775b2d27c4b50ab40c
Rajko Dervisevic <rajkod@hotmail.com>
2019-07-16 14:04:03 +0200

    Chunk >> Buffer in config.cpp

--- src/config.cpp
+++ src/config.cpp
@@ -2,7 +2,7 @@
 
 using namespace maolan;
 
-size_t Config::audioChunkSize = 0;
+size_t Config::audioBufferSize = 0;
 int Config::samplerate = 48000;
 int Config::bpm = 120;
 std::vector<BPM> Config::bpms;
