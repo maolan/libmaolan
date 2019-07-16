d30b000a1907f54fedf2fa54ca1821680b9f2c4c
Rajko Dervisevic <rajkod@hotmail.com>
2019-07-16 16:55:06 +0200

    PlayHead Check

--- maolan/audio/clip.h
+++ maolan/audio/clip.h
@@ -18,14 +18,18 @@ public:
   void process();
   std::size_t channels() const;
   uint64_t offset();
-  void offset(const uint64_t &offset);
+  void offset(const uint64_t &argOffset);
   uint64_t start();
-  void start(const uint64_t &start);
+  void start(const uint64_t &argStart);
+  uint64_t end();
+  void end(const uint64_t &argEnd);
   Buffer pull(const unsigned &channel);
+  uint64_t playHeadCheck();
 
 protected:
   uint64_t _offset;
   uint64_t _start;
+  uint64_t _end;
   bool initialized = false;
   File file;
 };
--- src/audio/clip.cpp
+++ src/audio/clip.cpp
@@ -6,7 +6,7 @@ using namespace maolan::audio;
 
 
 Clip::Clip(const uint64_t &start, const uint64_t &offset,
-            const std::string &path)
+           const std::string &path)
     : IO(0, true), _offset{offset}, file(path)
 {
   _type = "Clip";
@@ -25,10 +25,24 @@ std::size_t Clip::channels() const { return file.channels(); }
 
 uint64_t Clip::offset() { return _offset; }
 
-void Clip::offset(const uint64_t &offset) { _offset = offset; }
+void Clip::offset(const uint64_t &argOffset) { _offset = argOffset; }
 
 uint64_t Clip::start() { return _start; }
 
-void Clip::start(const uint64_t &start) { _start = start; }
+void Clip::start(const uint64_t &argStart) { _start = argStart; }
+
+uint64_t Clip::end() { return _end; }
+
+void Clip::end(const uint64_t &argEnd) { _start = argEnd; }
 
 Buffer Clip::pull(const unsigned &channel) { return file.pull(channel); }
+
+uint64_t Clip::playHeadCheck()
+{
+  if (playHead() < start())
+  {
+    return playHead();
+  }
+  else
+    return start();
+};
