34bfe04149b34d429be368991a4674e980b31329
Goran Mekić <meka@tilda.center>
2020-04-09 10:28:23 +0200

    Implement PluginPort with types

---
+++ maolan/audio/pluginport.h
@@ -0,0 +1,58 @@
+#include <lilv/lilv.h>
+
+
+namespace maolan::audio
+{
+enum class PluginPortDirection
+{
+  input,
+  output
+};
+
+
+enum class PluginPortType
+{
+  atom,
+  audio,
+  control,
+  cv,
+  event,
+  midiEvent,
+};
+
+
+class PluginPort
+{
+  public:
+    PluginPort(
+      LilvWorld *world,
+      const LilvPlugin *rawPlugin,
+      const LilvPort *p,
+      const float &minimum,
+      const float &maximum,
+      const float &defaultValue
+    );
+    ~PluginPort();
+
+    PluginPortDirection direction() { return _direction; }
+    PluginPortType type() { return _type; }
+
+  protected:
+    static LilvNode *lv2_AtomPort;
+    static LilvNode *lv2_AudioPort;
+    static LilvNode *lv2_ControlPort;
+    static LilvNode *lv2_CVPort;
+    static LilvNode *lv2_EventPort;
+    static LilvNode *lv2_InputPort;
+    static LilvNode *lv2_MidiEvent;
+    static LilvNode *lv2_OutputPort;
+    static LilvNode *lv2_ConnectionOptional;
+
+    float _minimum;
+    float _maximum;
+    float _default;
+    PluginPortDirection _direction;
+    PluginPortType _type;
+    bool _optional;
+};
+} // namespace maolan::audio
---
+++ src/audio/pluginport.cpp
@@ -0,0 +1,62 @@
+#include <maolan/audio/pluginport.h>
+#include <iostream>
+
+
+using namespace maolan::audio;
+
+
+LilvNode *PluginPort::lv2_AtomPort = nullptr;
+LilvNode *PluginPort::lv2_AudioPort = nullptr;
+LilvNode *PluginPort::lv2_ControlPort = nullptr;
+LilvNode *PluginPort::lv2_CVPort = nullptr;
+LilvNode *PluginPort::lv2_EventPort = nullptr;
+LilvNode *PluginPort::lv2_InputPort = nullptr;
+LilvNode *PluginPort::lv2_MidiEvent = nullptr;
+LilvNode *PluginPort::lv2_OutputPort = nullptr;
+LilvNode *PluginPort::lv2_ConnectionOptional = nullptr;
+
+
+PluginPort::PluginPort(
+  LilvWorld *world,
+  const LilvPlugin *rawPlugin,
+  const LilvPort *p,
+  const float &argMinimum,
+  const float &argMaximum,
+  const float &argDdefault
+)
+{
+  if (lv2_AtomPort == nullptr)
+  {
+    lv2_AtomPort = lilv_new_uri(world, LILV_URI_ATOM_PORT);
+    lv2_AudioPort = lilv_new_uri(world, LILV_URI_AUDIO_PORT);
+    lv2_ControlPort = lilv_new_uri(world, LILV_URI_CONTROL_PORT);
+    lv2_CVPort = lilv_new_uri(world, LILV_URI_CV_PORT);
+    lv2_EventPort = lilv_new_uri(world, LILV_URI_EVENT_PORT);
+    lv2_InputPort = lilv_new_uri(world, LILV_URI_INPUT_PORT);
+    lv2_MidiEvent = lilv_new_uri(world, LILV_URI_MIDI_EVENT);
+    lv2_OutputPort = lilv_new_uri(world, LILV_URI_OUTPUT_PORT);
+    lv2_ConnectionOptional = lilv_new_uri(world, LV2_CORE__connectionOptional);
+  }
+
+  if (lilv_port_is_a(rawPlugin, p, lv2_InputPort))
+  {
+    _direction = PluginPortDirection::input;
+  }
+  else if (lilv_port_is_a(rawPlugin, p, lv2_OutputPort))
+  {
+    _direction = PluginPortDirection::output;
+  }
+
+  if (lilv_port_is_a(rawPlugin, p, lv2_AudioPort))
+  {
+    _type = PluginPortType::audio;
+  }
+  else if (lilv_port_is_a(rawPlugin, p, lv2_ControlPort))
+  {
+    _type = PluginPortType::control;
+  }
+  else if (lilv_port_is_a(rawPlugin, p, lv2_AtomPort))
+  {
+    _type = PluginPortType::atom;
+  }
+}
