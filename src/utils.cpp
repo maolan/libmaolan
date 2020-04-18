1acc54369de8ba9d305ac1fe6514ee4bc993eb88
Goran Mekić <meka@tilda.center>
2020-04-18 15:20:09 +0200

    Comment out utils.cpp portions with OSS* classes

--- src/utils.cpp
+++ src/utils.cpp
@@ -15,59 +15,59 @@ IO *xmlElement2IO(pugi::xml_node *n)
 {
   audio::IO *io = nullptr;
 
-  std::string name = n->name();
-  if (name == "track")
-  {
-    io = new audio::Track(n->attribute("name").value(), 1);
-    return io;
-  }
-  else if (name == "clip")
-  {
-    io = new audio::Clip(
-      n->attribute("path").value(),
-      n->attribute("start").as_uint(),
-      n->attribute("end").as_uint(),
-      n->attribute("offset").as_uint()
-    );
-    for (auto parent = IO::begin(); parent != nullptr; parent = parent->next())
-    {
-      if (parent->name() == n->parent().parent().attribute("name").value())
-      {
-        io->parent(parent);
-        break;
-      }
-    }
-    return io;
-  }
-  else if (name == "input")
-  {
-    io = new audio::OSSIn(n->attribute("device").value(),
-                          n->attribute("channels").as_int());
-    return io;
-  }
-  else if (name == "output")
-  {
-    io = new audio::OSSOut(n->attribute("device").value(),
-                           n->attribute("channels").as_int());
-    return io;
-  }
-  else if (name == "connection")
-  {
-    maolan::audio::OSSOut *from = nullptr;
-    maolan::audio::Track *to = nullptr;
-    for (auto item = IO::begin(); item != nullptr; item = item->next())
-    {
-      if (item->type() == n->attribute("from").value())
-      {
-        from = (maolan::audio::OSSOut *)item;
-      }
-      if (item->name() == n->attribute("to").value())
-      {
-        to = dynamic_cast<maolan::audio::Track *>(item);
-      }
-    }
-    from->connect(to);
-  }
+  // std::string name = n->name();
+  // if (name == "track")
+  // {
+    // io = new audio::Track(n->attribute("name").value(), 1);
+    // return io;
+  // }
+  // else if (name == "clip")
+  // {
+    // io = new audio::Clip(
+      // n->attribute("path").value(),
+      // n->attribute("start").as_uint(),
+      // n->attribute("end").as_uint(),
+      // n->attribute("offset").as_uint()
+    // );
+    // for (auto parent = IO::begin(); parent != nullptr; parent = parent->next())
+    // {
+      // if (parent->name() == n->parent().parent().attribute("name").value())
+      // {
+        // io->parent(parent);
+        // break;
+      // }
+    // }
+    // return io;
+  // }
+  // else if (name == "input")
+  // {
+    // io = new audio::OSSIn(n->attribute("device").value(),
+                          // n->attribute("channels").as_int());
+    // return io;
+  // }
+  // else if (name == "output")
+  // {
+    // io = new audio::OSSOut(n->attribute("device").value(),
+                           // n->attribute("channels").as_int());
+    // return io;
+  // }
+  // else if (name == "connection")
+  // {
+    // maolan::audio::OSSOut *from = nullptr;
+    // maolan::audio::Track *to = nullptr;
+    // for (auto item = IO::begin(); item != nullptr; item = item->next())
+    // {
+      // if (item->type() == n->attribute("from").value())
+      // {
+        // from = (maolan::audio::OSSOut *)item;
+      // }
+      // if (item->name() == n->attribute("to").value())
+      // {
+        // to = dynamic_cast<maolan::audio::Track *>(item);
+      // }
+    // }
+    // from->connect(to);
+  // }
   return io;
 }
 
