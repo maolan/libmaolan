626ecf9b7e1dea990fe1f3a1d10a915f03c8b849
Goran Mekić <meka@tilda.center>
2023-07-17 15:35:58 +0200

    Cleanup

--- maolan/config.hpp
+++ maolan/config.hpp
@@ -12,16 +12,11 @@ namespace maolan
 class Config
 {
 public:
-  static std::size_t audioBufferSize;
+  static size_t audioBufferSize;
   static unsigned samplerate;
-  static std::size_t tempoIndex;
+  static size_t tempoIndex;
   static std::vector<Tempo> tempos;
   static std::uint16_t division;
   static std::filesystem::path root;
-
-  std::size_t count;
-  std::string name;
-  std::string device;
-  std::size_t sampleSize;
 };
 } // namespace maolan
